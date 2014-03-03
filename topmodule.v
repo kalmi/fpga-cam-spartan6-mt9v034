a4427f029946e60269cc28ebc04bc9db37d4cd78
Kálmán Tarnay <kalmi@users.noreply.github.com>
2014-03-03 14:27:27 +0100

    echo test

--- topmodule.v
+++ topmodule.v
@@ -8,14 +8,15 @@ module topmodule(
 	);
 
 reg [7:0] r;
-reg data_ready;
+wire [7:0] received_data;
+reg pending_send = 0;
 
 uart_send uut2 (
 	.RST(RST), 
 	.CLK(CLK), 
 	.TXD(TXD),
 	.DATA(r), 
-	.DATA_READY(data_ready), 
+	.DATA_READY(pending_send), 
 	.IDLE(sender_idle)
 );
 
@@ -23,52 +24,23 @@ uart_receive uut3 (
 	.RST(RST), 
 	.CLK(CLK), 
 	.RXD(RXD),
-	.DATA(), 
+	.DATA(received_data), 
 	.RXD_READY(rxd_ready)
 );
-
-reg start;
 
 reg prev_rxd_ready;
 
 always@(posedge CLK)
 begin
 	prev_rxd_ready <= rxd_ready;
-	if(RST)
-		start <= 0;
-	else if(rxd_ready&&!prev_rxd_ready)
-		start <= !start;
-	else
-		start <= start&&(r <= "9");
-end
-
-always@(posedge CLK)
-begin
-	if(RST)
+	
+	if(rxd_ready&&!prev_rxd_ready)
 	begin
-		r <= "0"-1;
-		data_ready <= 0;
+		pending_send<=1;
+		r<=received_data;
 	end
 	else
-	if(start)
-	begin
-		if(sender_idle)
-		begin
-			data_ready <= (r>="0"-1) && start;
-			if(r < "9")
-			begin
-				r <= r + 1'b1;
-			end
-			else
-				r <= "0";
-		end
-		else
-		begin
-			data_ready <= 0;
-		end
-	end
-	else
-		data_ready <= 0;
+		pending_send<=0;
 end
  
 endmodule
