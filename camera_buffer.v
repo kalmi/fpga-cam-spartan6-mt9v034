0025c37e2dd39268530c87a4d8d7add5cda99621
Kálmán Tarnay <kalmi@users.noreply.github.com>
2014-03-08 03:55:50 +0100

    line_buffer+test

---
+++ camera_buffer.v
@@ -0,0 +1,60 @@
+`timescale 1ns / 1ps
+
+module line_buffer #(parameter H = 752, parameter V = 480) ( 
+	input CLK,
+	input VALID_DATA,
+	input [$clog2(V)-1:0] CURRENT_COLUMN,
+	input [$clog2(H)-1:0] CURRENT_LINE,
+	input [$clog2(H)-1:0] INTERESTING_LINE,
+	input [9:0] DATA_IN,
+	input [$clog2(V)-1:0] READ_ADDRESS,
+	input RESET_READY_FLAG,
+	output WHOLE_LINE_READY_FLAG,
+	output reg [9:0] DATA_OUT);
+
+reg [9:0] mem[V-1:0];
+
+
+localparam UNINTERESTED = 2'b00;
+localparam RECORDING    = 2'b01;
+localparam READY        = 2'b10;
+localparam UNCLEAN      = 2'b11;
+
+reg [1:0] prev_state = UNCLEAN;
+
+reg [1:0] state;
+
+always @ (*)
+begin 
+	case(prev_state)
+		UNINTERESTED: state = (line_is_interesting ? RECORDING : UNINTERESTED);
+		RECORDING:    state = (line_is_interesting ? RECORDING : READY); 
+		READY:        state = (RESET_READY_FLAG    ? UNCLEAN   : READY);
+		UNCLEAN:      state = (line_is_interesting ? UNCLEAN   : UNINTERESTED);
+	endcase
+end
+
+always @ (posedge CLK)
+begin
+	prev_state <= state;
+end
+
+assign WHOLE_LINE_READY_FLAG = (state == READY);
+
+wire line_is_interesting = (CURRENT_LINE==INTERESTING_LINE);
+
+
+wire [$clog2(V)-1:0] read_address = READ_ADDRESS;
+wire [$clog2(V)-1:0] write_address = CURRENT_COLUMN;
+
+always @(posedge CLK) begin
+	if(VALID_DATA && state == RECORDING)
+	begin
+		DATA_OUT <= mem[read_address];
+		mem[write_address] <= DATA_IN;
+	end
+	else
+		DATA_OUT <= mem[read_address];
+end
+
+endmodule
---
+++ line_buffer_test.v
@@ -0,0 +1,117 @@
+`timescale 1ns / 1ps
+
+module line_buffer_test;
+
+	// Inputs
+	reg PIXCLK;
+	reg LINE_VALID;
+	reg FRAME_VALID;
+	reg [9:0] DATA_IN;
+
+	// Outputs
+	wire [9:0] CAMERA_DATA_OUT;
+	wire [1:0] CURRENT_LINE;
+	wire [0:0] CURRENT_COLUMN;
+	wire PIXEL_VALID;
+
+	// Instantiate the Unit Under Test (UUT)
+	camera #(3,2) uut1 (
+		.PIXCLK(PIXCLK), 
+		.LINE_VALID(LINE_VALID), 
+		.FRAME_VALID(FRAME_VALID), 
+		.DATA_IN(DATA_IN), 
+		.DATA_OUT(CAMERA_DATA_OUT), 
+		.CURRENT_LINE(CURRENT_LINE), 
+		.CURRENT_COLUMN(CURRENT_COLUMN), 
+		.PIXEL_VALID(PIXEL_VALID)
+	);
+
+
+	// New inputs
+	reg CLK;
+	reg [0:0] INTERESTING_LINE;
+	reg [0:0] READ_ADDRESS;
+	reg RESET_READY_FLAG;
+
+	// Outputs
+	wire WHOLE_LINE_READY_FLAG;
+	wire [9:0] DATA_OUT;
+
+	// Instantiate the Unit Under Test (UUT)
+	line_buffer #(3,2) uut2 (
+		.CLK(CLK), 
+		.VALID_DATA(PIXEL_VALID), 
+		.CURRENT_COLUMN(CURRENT_COLUMN), 
+		.CURRENT_LINE(CURRENT_LINE), 
+		.INTERESTING_LINE(1), //Line #1 
+		.DATA_IN(CAMERA_DATA_OUT), 
+		.READ_ADDRESS(READ_ADDRESS), 
+		.RESET_READY_FLAG(RESET_READY_FLAG), 
+		.WHOLE_LINE_READY_FLAG(WHOLE_LINE_READY_FLAG), 
+		.DATA_OUT(DATA_OUT)
+	);
+
+
+	initial begin
+		// Initialize Inputs
+		
+		PIXCLK = 0;
+		INTERESTING_LINE = 1;
+		DATA_IN = 0;
+		READ_ADDRESS = 0;
+		RESET_READY_FLAG = 0;
+
+	end
+	
+	
+	initial begin
+		// Initialize Inputs
+		CLK = 1;
+		PIXCLK = 0;
+		LINE_VALID = 1;
+		FRAME_VALID = 1;
+		DATA_IN = 0;
+		// Test ignoring of random already ongoing frame
+		#42;
+		LINE_VALID = 0;
+		FRAME_VALID = 0;
+		#20;
+		
+		FRAME_VALID = 1;
+		#10;
+		
+		LINE_VALID = 1;
+		DATA_IN = 11;
+		#10
+		LINE_VALID = 1;
+		DATA_IN = 12;
+		#10
+		LINE_VALID = 0;
+		#10
+		
+		LINE_VALID = 1;
+		DATA_IN = 21;
+		#10
+		LINE_VALID = 1;
+		DATA_IN = 22;
+		#10
+		LINE_VALID = 0;
+		#10
+		
+		LINE_VALID = 1;
+		DATA_IN = 31;
+		#10
+		LINE_VALID = 1;
+		DATA_IN = 32;
+		#10
+		LINE_VALID = 0;
+		#10
+		
+		FRAME_VALID = 0;
+
+	end
+      
+	always #5 PIXCLK <= ~PIXCLK;
+  always #5 CLK <= ~CLK;
+endmodule
+
